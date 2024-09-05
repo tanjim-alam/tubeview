3beb3853bface43fbd936bbe4a01114ee7d58ccf
Tanjim Alam <122804862+Tanjim099@users.noreply.github.com>
2024-09-06 00:52:16 +0530

    added fb followers page

---
+++ src/app/buy-facebook-followers/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import FacebookFollowersPage from './FacebookFollowersPage';
+export const generateMetadata = () => {
+    return {
+        title: "Buy Real Facebook Followers -  Tubeviews",
+        description: "Buy Real Facebook Followers -  Tubeviews"
+    }
+}
+
+function page() {
+    return <FacebookFollowersPage />
+}
+
+export default page
--- src/app/components/ServiceCard.jsx
+++ src/app/components/ServiceCard.jsx
@@ -118,7 +118,7 @@ function ServiceCard({ packageData }) {
         <div className='flex flex-col lg:flex-row'>
             <div className='lg:w-[80%] w-full m-auto border p-2 lg:p-10 mt-7 relative rounded-2xl shadow-[0_2px_2px_rgba(0,0,0,0.10)]'>
                 <div className='flex justify-center items-center'>
-                    <p className=' absolute top-[-19px] sm:left-[35%] lg:top-[-26px] lg:left-[40%] bg-white border py-1 px-4 lg:py-2 text-black lg:px-8 border-black rounded-md  text-lg font-semibold'>{packageData.serviceName || "Real YouTube Views"}</p>
+                    <p className=' absolute top-[-19px]  lg:top-[-26px] bg-white border py-1 px-4 lg:py-2 text-black lg:px-8 border-black rounded-md  text-lg font-semibold'>{packageData.serviceName || "Real YouTube Views"}</p>
                 </div>
                 <div className='flex flex-col gap-4'>
                     <div className='lg:flex sm:flex grid grid-cols-2 w-full justify-between text-sm font-medium border-b border-black p-5 lg:bg-red-100 bg-primary rounded-2xl'>
