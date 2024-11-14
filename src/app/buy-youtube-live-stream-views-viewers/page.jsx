a3027519a3653c1dc3cc1780d7894514e76d72d1
Tanjim Alam <122804862+Tanjim099@users.noreply.github.com>
2024-11-14 22:31:36 +0530

    updated yt live views page

--- src/app/buy-youtube-live-stream-views-viewers/page.jsx
+++ src/app/buy-youtube-live-stream-views-viewers/page.jsx
@@ -5,7 +5,7 @@ import YouTubeLiveViewsPage from './YouTubeLiveViewsPage';
 export const generateMetadata = () => {
     return {
         title: "Buy YouTube Live Views with Instant 100% Real Active- TubeViews",
-        description: "Buy YouTube Live Views to Boost your stream's visibility. Attract more viewers, increase engagement, and enhance your live stream's performance instantly.",
+        description: "Buy YouTube Live Views to Boost your streams visibility. Attract more viewers, increase engagement, and enhance your live streams performance instantly.",
         keywords: "Buy YouTube Live Views, Buy Real YouTube Live Views, Buy Active YouTube Live Views",
     }
 }
