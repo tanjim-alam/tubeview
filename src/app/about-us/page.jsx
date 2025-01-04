95299268be377a2b2cea1eccfe6c4cec5743977a
Tanjim Alam <122804862+Tanjim099@users.noreply.github.com>
2025-01-04 21:08:39 +0530

    update

--- src/app/about-us/page.jsx
+++ src/app/about-us/page.jsx
@@ -4,7 +4,10 @@ export const generateMetadata = () => {
     return {
         title: "About Us - TubeViews Media",
         description: "TubeVIews is Unique Social Media Company. They Providing Real and Active Video Views, Subscribers, Comments  Likes, Followers, and More. You can Buy cheap Social Media Services.We are a dedicated company that strives to make your social media marketing easy and convenient. This we do by selling you followers and likes, which form the standards",
-        keywords: "TubeViews AboutUs, Buy YouTube Views"
+        keywords: "TubeViews AboutUs, Buy YouTube Views",
+        alternates: {
+            canonical: 'https://www.gofamy.com/about-us',
+        }
     }
 }
 
