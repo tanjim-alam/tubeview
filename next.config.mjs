d295043ef048716a2fe55003a57d6f6a77e52e48
Tanjim Alam <122804862+Tanjim099@users.noreply.github.com>
2025-01-07 21:43:21 +0530

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: [],
+        domains: [], // Add your allowed image domains here if needed
         deviceSizes: [320, 420, 768, 1024, 1200], // Device sizes for responsive images
         imageSizes: [16, 32, 48, 64, 96], // Sizes for srcset
-    }
+    },
+
+    // Add redirects to redirect traffic from gofamy.com to www.gofamy.com
+    async redirects() {
+        return [
+            {
+                source: '/',
+                has: [
+                    {
+                        type: 'host',
+                        value: 'gofamy.com', // Check if the request is from gofamy.com
+                    },
+                ],
+                destination: 'https://www.gofamy.com', // Redirect to the www version
+                permanent: true, // Use a permanent 301 redirect
+            },
+        ];
+    },
 };
 
 export default nextConfig;
